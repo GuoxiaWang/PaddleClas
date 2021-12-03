52bec7feec3907a549d049ec3fefcbf96ecb0f44
GuoxiaWang <mingzilaochongtu@gmail.com>
2021-12-03 23:00:51 +0800

    fix vit eval param

--- tools/train.sh
+++ tools/train.sh
@@ -4,6 +4,6 @@
 # python3.7 tools/train.py -c ./ppcls/configs/ImageNet/ResNet/ResNet50.yaml
 
 # for multi-cards train
-python -m paddle.distributed.launch --ips=$TRAINER_IP_LIST --gpus="0,1,2,3,4,5,6,7" tools/train.py -c ./ppcls/configs/ImageNet/DeiT/DeiT_base_patch16_224.yaml
+#python -m paddle.distributed.launch --ips=$TRAINER_IP_LIST --gpus="0,1,2,3,4,5,6,7" tools/train.py -c ./ppcls/configs/ImageNet/DeiT/DeiT_base_patch16_224.yaml
 #python -m paddle.distributed.launch --ips=$TRAINER_IP_LIST --gpus="0,1,2,3,4,5,6,7" tools/train.py -c ./ppcls/configs/ImageNet/DeiT/DeiT_base_patch16_224_pretrained_384_finetune.yaml
-#python -m paddle.distributed.launch --ips=$TRAINER_IP_LIST --gpus="0,1,2,3,4,5,6,7" tools/train.py -c ./ppcls/configs/ImageNet/VisionTransformer/ViT_base_patch16_224.yaml
+python -m paddle.distributed.launch --ips=$TRAINER_IP_LIST --gpus="0,1,2,3,4,5,6,7" tools/train.py -c ./ppcls/configs/ImageNet/VisionTransformer/ViT_base_patch16_224.yaml
